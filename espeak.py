7716db38153d94ab709d87f587044c1de30dc1d4
Electronica_DonBosco <98735682+Mendix28@users.noreply.github.com>
2022-02-17 19:04:14 +0100

    Delete espeak.py

--- espeak.py
+++
@@ -1,8 +0,0 @@
-import os
-import pyttsx3
-engine=pyttsx3.init()
-engine.setProperty('rate',125)
-engine.setProperty('voice','english+f3')
-text='Hi!, I am BLINDA'
-engine.say(text)
-engine.runAndWait()
