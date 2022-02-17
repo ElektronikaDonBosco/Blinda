9d648d4341d1f43154867d147e610db51c51d37b
Electronica_DonBosco <98735682+Mendix28@users.noreply.github.com>
2022-02-17 19:03:21 +0100

    Update espeak.py

--- espeak.py
+++ espeak.py
@@ -1,1 +1,8 @@
-
+import os
+import pyttsx3
+engine=pyttsx3.init()
+engine.setProperty('rate',125)
+engine.setProperty('voice','english+f3')
+text='Hi!, I am BLINDA'
+engine.say(text)
+engine.runAndWait()
